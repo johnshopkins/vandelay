5fdcf374009217319aaf1b6d25e6c72e3a6e1dfb
Jen Wachter <jenleighkelly@gmail.com>
2013-11-25 16:24:04 -0500

    First stab at options page.

--- vandelay.php
+++ vandelay.php
@@ -8,6 +8,8 @@ Version: 0.1
 
 $path = plugin_dir_path(__FILE__);
 
+
+// load vandelay command
 if ( defined("WP_CLI") && WP_CLI ) {
 	include $path . "VandelayCommand.php";
 	include $path . "workers/Worker.php";
@@ -17,4 +19,39 @@ if ( defined("WP_CLI") && WP_CLI ) {
 	foreach ($files as $file) {
 		include_once $path . "workers/{$file}";
 	}
+}
+
+
+// create vandelay settings page
+add_action("admin_menu", function() {
+	add_submenu_page("options-general.php", "Vandelay options", "Vandelay", "activate_plugins", "vandelay", "vandelay_options_display");
+});
+
+function vandelay_options_display() {  
+?>  
+    <!-- Create a header in the default WordPress 'wrap' container -->  
+    <div class="wrap">  
+      
+        <div id="icon-themes" class="icon32"></div>  
+        <h2>Vandelay Options</h2>  
+        <?php settings_errors(); ?>  
+          
+        <form method="post" action="options.php">  
+  
+            <?php settings_fields( 'vandelay' ); ?>  
+            <?php do_settings_sections( 'vandelay' ); ?>   
+          
+            <?php submit_button(); ?>  
+              
+        </form>  
+          
+    </div><!-- /.wrap -->  
+<?php  
+}
+
+
+// load settings
+$files = array_diff(scandir($path. "settings"), array("..", "."));
+foreach ($files as $file) {
+	include_once $path . "settings/{$file}";
 }
