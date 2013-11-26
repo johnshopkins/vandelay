7d1ceb33d910c1b2f4bc705d9c93d7da12440885
Jen Wachter <jenleighkelly@gmail.com>
2013-11-26 16:22:09 -0500

    Modified validation function to remove trailing slashes.

--- fields/config.php
+++ fields/config.php
@@ -6,17 +6,26 @@ return array(
 		"type" => "text",
 		"label" => "Configuration Directory (absolute)",
 		"validation" => function ($input) {
-			$file = $input . "/test.json";
+
+			// make sure there is not a slash at the end
+			if (substr($input, -1) === "/") {
+				$sanitized = substr($input, 0, strlen($input) -1);
+			} else {
+				$sanitized = $input;
+			}
+
+			$file = $sanitized . "/test.json";
 			$fopen = fopen($file, "w");
 			
 			if (!$fopen) {
-				$message = "The directory `{$input}` is not writable by WordPress. Please make this directory writable or choose a different directory.";
+				$message = "The directory `{$sanitized}` is not writable by WordPress. Please make this directory writable or choose a different directory.";
 				add_settings_error("vandelay_config_url", "not-writable", $message);
 			} else {
 				fclose($fopen);
+				unlink($file);
 			}
 
-			return apply_filters(__FUNCTION__, $input, $input);  
+			return apply_filters(__FUNCTION__, $sanitized, $input);  
 		}
 	)
 
